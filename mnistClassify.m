7bac2a42873894ecd3c6c6252efc875bbf6768ef
mandy <mwilkens241@gmail.com>
2018-11-19 12:18:52 -0500

    added blank files for mnist classifier

--- mnistClassify.m
+++ mnistClassify.m
@@ -1,1 +1,6 @@
-% This is a blank file
+% This file will classify a 50x50 normalized 2D array into a text letter
+% will also output an uncertainty loss
+function [ltr, loss] = mnistClassify(array)
+    ltr = sum(array);
+    loss = 0;
+end
---
+++ mnistTrain.m
@@ -0,0 +1,6 @@
+% This function will train the mnistModel with training and
+% test data, will output the loss
+function [mdl, loss] = mnistTrain(train, test)
+    mdl = train;
+    loss = test;
+end
